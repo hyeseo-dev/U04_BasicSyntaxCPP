43f82ce34dbe2678b0acc526d26aa5ef1f80df53
hyeseo <hyeseo.dev@gmail.com>
2024-06-13 00:01:32 +0900

    feat. Chest

---
+++ Source/ThirdPersonCPP/Assiment/CChestBase.cpp
@@ -0,0 +1,33 @@
+#include "CChestBase.h"
+#include "Materials/MaterialInstanceConstant.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "Global.h"
+
+ACChestBase::ACChestBase()
+{
+	TopMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TopMeshComp"));
+	BottomMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BottomMeshComp"));
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> topMeshAsset(TEXT("/Game/Assiment/Chest/SM_ChestTop"));
+	ConstructorHelpers::FObjectFinder<UStaticMesh> bottomMeshAsset(TEXT("/Game/Assiment/Chest/SM_ChestBottom"));
+
+	if (topMeshAsset.Succeeded() & bottomMeshAsset.Succeeded())
+	{
+		TopMeshComp->SetStaticMesh(topMeshAsset.Object);
+		BottomMeshComp->SetStaticMesh(bottomMeshAsset.Object);
+
+		BottomMeshComp->SetupAttachment(TopMeshComp);
+	}
+}
+
+void ACChestBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UMaterialInstanceConstant* materialAsset = Cast<UMaterialInstanceConstant>(StaticLoadObject(UMaterialInstanceConstant::StaticClass(), nullptr, TEXT("/Game/Assiment/Chest/MI_Chest")));
+	if (materialAsset)
+	{
+		TopDynamicMaterial = UMaterialInstanceDynamic::Create(materialAsset, nullptr);
+		TopMeshComp->SetMaterial(0, TopDynamicMaterial);
+	}
+}
---
+++ Source/ThirdPersonCPP/Assiment/CChestBase.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CChestBase.generated.h"
+
+
+UCLASS()
+class THIRDPERSONCPP_API ACChestBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ACChestBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly)
+	UStaticMeshComponent* TopMeshComp;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	UStaticMeshComponent* BottomMeshComp;
+
+	UMaterialInstanceDynamic* TopDynamicMaterial;
+};
